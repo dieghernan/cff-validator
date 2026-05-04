10d80d670d582485aa28e788e8f68ed6b7b7c7d4
Diego H. <diego.hernangomezherrero@gmail.com>
2026-05-04 08:28:40 +0200

    Add new validator

--- docker/validate_cff.R
+++ docker/validate_cff.R
@@ -1,28 +1,18 @@
 # Modified version from pharmaverse/admiralci
 # See https://github.com/pharmaverse/admiralci/tree/61347fe11955297818b3ca7814fc7328f2ad7840/.github/actions/cran-status-extract
 
-
 # 0. Setup ----
-if (!requireNamespace("cffr", quietly = TRUE)) {
-  suppressMessages(install.packages("cffr",
-    repos = "https://cloud.r-project.org",
-    verbose = FALSE,
-    quiet = TRUE,
-    dependencies = TRUE
-  ))
-}
-if (!requireNamespace("optparse", quietly = TRUE)) {
-  suppressMessages(install.packages("optparse",
-    repos = "https://cloud.r-project.org",
-    verbose = FALSE,
-    quiet = TRUE
-  ))
-}
+
+install.packages("pak")
+
+pak::pak("cffr", dependencies = TRUE, ask = FALSE)
+pak::pak("optparse", dependencies = TRUE, ask = FALSE)
 
 # check if needed : package name and working dir path as input arguments :
 library(optparse)
 option_list <- list(
-  make_option(c("-p", "--cffpath"),
+  make_option(
+    c("-p", "--cffpath"),
     type = "character",
     help = "package name (REQUIRED)",
     metavar = "character"
@@ -34,16 +24,22 @@ cffpath <- opt$cffpath
 
 
 # clean files
-if (file.exists("citation_cff.md")) unlink("citation_cff.md")
-if (file.exists("issue.md")) unlink("issue.md")
-
+if (file.exists("citation_cff.md")) {
+  unlink("citation_cff.md")
+}
+if (file.exists("issue.md")) {
+  unlink("issue.md")
+}
 
 # Validate cff ----
 
-
 cat("Validating cff\n")
-citfile <- yaml::read_yaml(cffpath)
-# All elements to character
+citfile <- cffr::cff_read(cffpath)
+
+# Convert to list
+citfile <- as.list(citfile)
+
+# This prevents errors with jsonvalidate
 citfile <- rapply(citfile, function(x) as.character(x), how = "replace")
 
 # Convert to json
@@ -53,38 +49,44 @@ cit_temp <- jsonlite::toJSON(citfile, pretty = TRUE, auto_unbox = TRUE)
 schema_local <- system.file("schema/schema.json", package = "cffr")
 
 # Validate
-result <- jsonvalidate::json_validate(cit_temp,
+result <- jsonvalidate::json_validate(
+  cit_temp,
   schema_local,
-  verbose = TRUE
+  verbose = TRUE,
+  engine = "ajv"
 )
 
 # Results
 if (result == FALSE) {
-  writeLines(paste0("\n:x: ", cffpath, " has errors\n"),
+  writeLines(
+    paste0("\n:x: ", cffpath, " has errors\n"),
     con = "citation_cff.md"
   )
+
   # Format outputs
   get_errors <- attr(result, "errors")
-  get_errors$field <- gsub(
-    "^data", "cff",
-    get_errors$field
-  )
+  field <- get_errors$instancePath
+  field[field == ""] <- "/"
+  field <- paste0("cff", field)
+  get_errors$field <- field
 
-  write(knitr::kable(get_errors, align = "l"),
+  write(
+    knitr::kable(get_errors[, c("field", "message")], align = "l"),
     file = "citation_cff.md",
     append = TRUE
   )
 
-  write("\n\nSee [Guide to Citation File Format schema version 1.2.0](https://github.com/citation-file-format/citation-file-format/blob/main/schema-guide.md) for debugging.",
+  write(
+    "\n\nSee [Guide to Citation File Format schema version 1.2.0](https://github.com/citation-file-format/citation-file-format/blob/main/schema-guide.md) for debugging.",
     file = "citation_cff.md",
     append = TRUE
   )
 
   cat(paste0(
-    "::warning::", cffpath,
+    "::warning::",
+    cffpath,
     " has errors, see Job Summary of this GH action for details.\n"
   ))
-
 
   cat(paste0(cffpath, " file not valid. See Job Summary."))
 
@@ -92,12 +94,15 @@ if (result == FALSE) {
 } else {
   writeLines(
     paste0(
-      "\n:white_check_mark: Congratulations! ", cffpath, " is valid\n"
+      "\n:white_check_mark: Congratulations! ",
+      cffpath,
+      " is valid\n"
     ),
     con = "citation_cff.md"
   )
   cat(paste0(
-    "::notice ::", cffpath,
+    "::notice ::",
+    cffpath,
     " is a valid CFF file.\n"
   ))
 }
--- examples/r-reprex.R
+++ examples/r-reprex.R
@@ -9,26 +9,29 @@ cit_temp <- jsonlite::toJSON(citfile, pretty = TRUE, auto_unbox = TRUE)
 
 # Download latest scheme
 schema_temp <- tempfile("schema", fileext = ".json")
-download.file("https://raw.githubusercontent.com/citation-file-format/citation-file-format/main/schema.json",
+download.file(
+  "https://raw.githubusercontent.com/citation-file-format/citation-file-format/main/schema.json",
   schema_temp,
-  mode = "wb", quiet = TRUE
+  mode = "wb",
+  quiet = TRUE
 )
 
 # Validate
-result <- jsonvalidate::json_validate(cit_temp,
-  schema_temp,
-  verbose = TRUE
-)
+result <- jsonvalidate::json_validate(cit_temp, schema_temp, verbose = TRUE)
 # Results
 message("------\n")
 if (result == FALSE) {
-  print(knitr::kable(attributes(result)$errors,
+  print(knitr::kable(
+    attributes(result)$errors,
     align = "l",
     caption = paste(citation_path, "errors:")
   ))
 
   message("\n\n------")
-  stop(citation_path, " file not valid. See Artifact: citation-cff-errors for details.")
+  stop(
+    citation_path,
+    " file not valid. See Artifact: citation-cff-errors for details."
+  )
 } else {
   message(citation_path, " is valid.")
   message("\n\n------")
